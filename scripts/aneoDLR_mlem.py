7f9c431df5a7dec897691a0e31aedc4f0e91b723
James Endicott <endicott.james@gmail.com>
2024-08-21 18:43:53 -0500

    Update aneoDLR_mlem.py
    Fix output directory.

--- scripts/aneoDLR_mlem.py
+++ scripts/aneoDLR_mlem.py
@@ -25,7 +25,7 @@ for offset in range(10):
 frame_order = [frames[f] for f in (8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8)]
 
 frames[9].save(
-    "png/aneoDLR_mlem.apng",
+    "aneoDLR_mlem.apng",
     save_all=True,
     append_images=[f for f in frame_order],
     loop=0,
